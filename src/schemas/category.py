4276c09d665d0c0ed4ac8d217229fc1207a51e2d
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-08-05 16:42:46 -0300

    feat: Add Category schema with slug validation

---
+++ src/schemas/category.py
@@ -0,0 +1,13 @@
+import re
+from pydantic import field_validator
+from schemas.base import CustomBaseModel
+
+class Category(CustomBaseModel):
+    name: str
+    slug: str
+    
+    @field_validator('slug')
+    def validate_slug(cls, value):
+        if not re.match('^([a-z]|-|_)+$', value):
+            raise ValueError('Invalid slug')
+        return value
