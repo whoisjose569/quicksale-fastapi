275bb4e1ba5d84e1b0aa0cb75f47681efe249a7e
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-08-05 15:17:55 -0300

    feat: add Category model to database schema

---
+++ src/db/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from db.base import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String, nullable=False)
+    slug = Column('slug', String, nullable=False)
