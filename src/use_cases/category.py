767af54ea32c7172d1cf1ddcf5e4d41756653ce7
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-08-12 16:21:06 -0300

    feat: implementing delete test in category use case

--- src/use_cases/category.py
+++ src/use_cases/category.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from db.models import Category as CategoryModel
 from schemas.category import Category, CategoryOutput
+from fastapi.exceptions import HTTPException
+from fastapi import status
 
 class CategoryUseCases:
     def __init__(self, db_session: Session):
@@ -18,6 +20,13 @@ class CategoryUseCases:
             for category_model in categories_on_db
         ]
         return categories_output
+    
+    def delete_category(self, id: int):
+        category_model = self.db_session.query(CategoryModel).filter_by(id=id).first()
+        if not category_model:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Category not found')
+        self.db_session.delete(category_model)
+        self.db_session.commit()
     
     def serialize_category(self, category_model: CategoryModel):
         return CategoryOutput(**category_model.__dict__)
