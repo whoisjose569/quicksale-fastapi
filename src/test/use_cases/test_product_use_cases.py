05c2c42301e1759c9c4d4e29856737965787491a
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-08-19 17:31:33 -0300

    feat: implementing update product and test update product

--- src/test/use_cases/test_product_use_cases.py
+++ src/test/use_cases/test_product_use_cases.py
@@ -52,3 +52,40 @@ def test_add_product_uc_invalid_category(db_session):
     
     with pytest.raises(HTTPException):
         uc.add_product(product=product, category_slug='invalid')
+
+def test_update_product(db_session):
+    category = CategoryModel(name='Roupa', slug='roupa')
+    db_session.add(category)
+    db_session.commit()
+    
+    product_on_db = ProductModel(name= 'Camisa Adidas', 
+                           slug= 'camisa-adidas',
+                           price=100.99,
+                           stock=20,
+                           category_id=category.id)
+    
+    db_session.add(product_on_db)
+    db_session.commit()
+
+    uc = ProductUseCases(db_session=db_session)
+    new_product = Product(
+        name= 'Camisa Nike',
+        slug= 'camisa-nike',
+        price = 22.99,
+        stock= 22
+    )
+    uc.update_product(id=product_on_db.id, product=new_product)
+    
+    product_updated_on_db = db_session.query(ProductModel).filter_by(id=product_on_db.id).first()
+    
+    assert product_updated_on_db is not None
+    assert product_updated_on_db.name == new_product.name
+    assert product_updated_on_db.slug == new_product.slug
+    assert product_updated_on_db.price == new_product.price
+    assert product_updated_on_db.stock == new_product.stock
+    
+    db_session.refresh(product_updated_on_db)
+    db_session.delete(product_updated_on_db)
+    db_session.delete(category)
+    db_session.commit()
+
