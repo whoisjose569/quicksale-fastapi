0f830d9f575ec66e74fdcd2082aab9e50aa3163c
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-08-26 18:18:54 -0300

    feat: implementing ProductOutput test schema

--- src/test/schemas/test_product_schema.py
+++ src/test/schemas/test_product_schema.py
@@ -1,5 +1,6 @@
 import pytest
-from schemas.product import Product, ProductInput
+from schemas.product import Product, ProductInput, ProductOutput
+from schemas.category import Category
 
 def test_product_schema():
     product = Product(
@@ -69,5 +70,28 @@ def test_product_input_schema():
             "slug": 'camisa-nike',
             "price" : 22.99,
             "stock" : 22
+        }
+    }
+
+def test_product_output_schema():
+    category = Category(name='Roupa', slug='roupa')
+    product_output = ProductOutput(
+        id = 1,
+        name = 'Camisa Nike',
+        slug = 'camisa-nike',
+        price = 22.99,
+        stock = 22,
+        category = category
+    )
+    
+    assert product_output.dict() == {
+        'id': 1,
+        'name': 'Camisa Nike',
+        'slug': 'camisa-nike',
+        'price': 22.99,
+        'stock': 22,
+        'category': {
+            'name': 'Roupa',
+            'slug': 'roupa'
         }
     }
