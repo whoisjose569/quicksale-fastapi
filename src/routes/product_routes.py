5bf8ee513755c637f37b2975ad536174865ae49a
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-08-28 16:39:26 -0300

    feat: implementing list product route with search

--- src/routes/product_routes.py
+++ src/routes/product_routes.py
@@ -28,9 +28,9 @@ def delete_product(id: int, db_session: Session = Depends(get_db_session)):
     return Response(status_code=status.HTTP_200_OK)
 
 @router.get('/list')
-def list_product(db_session: Session = Depends(get_db_session)):
+def list_product(search: str = '', db_session: Session = Depends(get_db_session)):
     uc = ProductUseCases(db_session=db_session)
-    products = uc.list_products()
+    products = uc.list_products(search=search)
     
     return products
     
