05eb34538c6121f00e561ea1c1af799d8589dfa1
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-09-03 15:30:11 -0300

    docs: update and improve documentation across multiple files

--- src/routes/category_routes.py
+++ src/routes/category_routes.py
@@ -1,24 +1,25 @@
+from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, Response, status
-from schemas.category import Category
+from schemas.category import Category, CategoryOutput
 from routes.deps import get_db_session
 from use_cases.category import CategoryUseCases
 
 router = APIRouter(prefix='/category', tags=['Category'])
 
-@router.post('/add')
+@router.post('/add', status_code=status.HTTP_201_CREATED, description="Add new category")
 def add_category(category: Category, db_session: Session = Depends(get_db_session)):
     uc = CategoryUseCases(db_session=db_session)
     uc.add_category(category=category)
     return Response(status_code=status.HTTP_201_CREATED)
 
-@router.get('/list')
+@router.get('/list', response_model=List[CategoryOutput], description="List categories")
 def list_category(db_session: Session = Depends(get_db_session)):
     uc = CategoryUseCases(db_session=db_session)
     response = uc.list_categories()
     return response
 
-@router.delete('/delete/{id}')
+@router.delete('/delete/{id}', description="Delete category")
 def delete_category(id: int, db_session: Session = Depends(get_db_session)):
     uc = CategoryUseCases(db_session=db_session)
     uc.delete_category(id=id)
--- src/routes/product_routes.py
+++ src/routes/product_routes.py
@@ -1,33 +1,34 @@
+from typing import List
 from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
 from routes.deps import get_db_session
 from use_cases.product import ProductUseCases
-from schemas.product import Product, ProductInput
+from schemas.product import Product, ProductInput, ProductOutput
 
-router = APIRouter(prefix='/product')
+router = APIRouter(prefix='/product', tags=['Product'])
 
-@router.post('/add')
+@router.post('/add', status_code=status.HTTP_201_CREATED, description="Add new product")
 def add_product(product_input: ProductInput, db_session: Session = Depends(get_db_session)):
     uc = ProductUseCases(db_session)
     uc.add_product(product=product_input.product,
                    category_slug=product_input.category_slug)
     return Response(status_code=status.HTTP_201_CREATED)
 
-@router.put('/update/{id}')
+@router.put('/update/{id}', description="Update product")
 def update_product(id: int, product: Product, db_session: Session = Depends(get_db_session)):
     uc = ProductUseCases(db_session=db_session)
     uc.update_product(id=id,product=product)
     
     return Response(status_code=status.HTTP_200_OK)
 
-@router.delete('/delete/{id}')
+@router.delete('/delete/{id}', description='Delete product')
 def delete_product(id: int, db_session: Session = Depends(get_db_session)):
     uc = ProductUseCases(db_session=db_session)
     uc.delete_product(id=id)
     
     return Response(status_code=status.HTTP_200_OK)
 
-@router.get('/list')
+@router.get('/list', response_model=List[ProductOutput], description='List products')
 def list_product(search: str = '', db_session: Session = Depends(get_db_session)):
     uc = ProductUseCases(db_session=db_session)
     products = uc.list_products(search=search)
