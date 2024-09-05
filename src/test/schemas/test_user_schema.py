e9f25d01f8c83f8353b924cd91182087a6dea400
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-09-05 17:24:01 -0300

    config: added test token data schema

--- src/test/schemas/test_user_schema.py
+++ src/test/schemas/test_user_schema.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 import pytest
-from schemas.user import User
+from schemas.user import User, TokenData
 
 def test_user_schema():
     user = User(username='Jose', password='pass')
@@ -12,4 +13,15 @@ def test_user_schema():
 def test_user_schema_invalid_username():
     with pytest.raises(ValueError):
         user = User(username='João#', password='pass')
+
+def test_token_date():
+    expires_at = datetime.now()
+    token_data = TokenData(
+        access_token = 'token',
+        expires_at = expires_at
+    )
     
+    assert token_data.dict() == {
+        'access_token': 'token',
+        'expires_at': expires_at
+    }
