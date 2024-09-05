c01d1ab1a49ea965cf13a8e2bdf1fdffe1820fa0
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-09-05 17:25:07 -0300

    feat: implementing user login use case

--- src/use_cases/user.py
+++ src/use_cases/user.py
@@ -1,12 +1,17 @@
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-from schemas.user import User
+from schemas.user import User, TokenData
 from db.models import User as UserModel
 from fastapi.exceptions import HTTPException
 from fastapi import status
 from sqlalchemy.exc import IntegrityError
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from decouple import config
 
 crypt_context = CryptContext(schemes=['sha256_crypt'], deprecated="auto")
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config('ALGORITHM')
 
 class UserUseCases:
     def __init__(self, db_session: Session) -> None:
@@ -21,6 +26,31 @@ class UserUseCases:
         self.db_session.add(user_on_db)
         try:
             self.db_session.commit()
-        except:
+        except IntegrityError:
             self.db_session.rollback()
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Username already exists')
+
+    def user_login(self, user: User, expires_in: int = 30):
+        user_on_db = self._get_user(username=user.username)
+        
+        if user_on_db is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Username os password does not exists')
+        
+        if not crypt_context.verify(user.password, user_on_db.password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Username os password does not exists')
+        
+        expires_at = datetime.utcnow() + timedelta(expires_in)
+        
+        data = {
+            'sub': user_on_db.username,
+            'exp': expires_at
+        }
+        
+        access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+        
+        token_data = TokenData(access_token=access_token, expires_at=expires_at)
+        return token_data
+    
+    def _get_user(self, username: str):
+        user_on_db = self.db_session.query(UserModel).filter_by(username=username).first()
+        return user_on_db
